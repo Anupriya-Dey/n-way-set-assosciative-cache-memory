842f7b93132a7488b65101d0e1f54bde58190627
Anupriya-Dey <96530004+Anupriya-Dey@users.noreply.github.com>
2023-03-23 00:40:34 +0530

    Clean code

--- main.v
+++ main.v
@@ -2,10 +2,7 @@ module memory();
     parameter MAIN_MEMORY_SIZE_EXP = 32'd24;
     parameter CACHE_SIZE_EXP = 32'd14;
     parameter WAYS_EXP = 32'd4; //New addition
-    // parameter WAYS = 32'd1<<WAYS_EXP; //New addition
-    parameter ADDRESS_SIZE = 32'd24;
-    // parameter BLOCK_SIZE = 32'd32;
-    // parameter BLOCK_SIZE_EXP = 32'd5;
+    parameter ADDRESS_SIZE = 32'd24;    
     parameter BLOCK_SIZE = 32'd64;
     parameter BLOCK_SIZE_EXP = 32'd6;
     parameter DATA_SIZE = 32'd8;
@@ -19,10 +16,7 @@ module memory();
     // integer CACHE_DATA_SIZE = WAYS_EXP + ADDRESS_SIZE - INDEX_SIZE_EXP + BLOCK_SIZE;
     integer INDEX_SIZE = 32'd1<<INDEX_SIZE_EXP;
 
-    // reg [DATA_SIZE-1:0] memory[32'd1<<(MAIN_MEMORY_SIZE_EXP-2):0][3:0];
     reg [DATA_SIZE-1:0] memory[32'd1<<(MAIN_MEMORY_SIZE_EXP-(BLOCK_SIZE_EXP-3)):0][32'd1<<(BLOCK_SIZE_EXP-3)-1:0];
-    // reg [32'd2 + ADDRESS_SIZE + CACHE_SIZE_EXP - (OFFSET_EXP + BLOCK_SIZE_EXP) + BLOCK_SIZE -1:0] cache [32'd1<<(CACHE_SIZE_EXP - (OFFSET_EXP + BLOCK_SIZE_EXP))-1:0][3:0];
-    // reg [WAYS_EXP + ADDRESS_SIZE + CACHE_SIZE_EXP - (WAYS_EXP + BLOCK_SIZE_EXP) + BLOCK_SIZE -1:0] cache [32'd1<<(CACHE_SIZE_EXP - (OFFSET_EXP + BLOCK_SIZE_EXP))-1:0][3:0];
     reg [32'd2 + ADDRESS_SIZE - CACHE_SIZE_EXP + (WAYS_EXP + BLOCK_SIZE_EXP) + BLOCK_SIZE -1:0] cache [(32'd1<<(CACHE_SIZE_EXP - (WAYS_EXP + BLOCK_SIZE_EXP)))-1:0][(32'd1<<WAYS_EXP) - 1:0];
     reg [ADDRESS_SIZE+3:0] instruction_set[0:NO_OF_INST-1];
     reg [ADDRESS_SIZE+3:0] instn;
@@ -31,14 +25,13 @@ module memory();
     reg [BLOCK_SIZE-1:0] data;
     reg [ADDRESS_SIZE - (CACHE_SIZE_EXP - (WAYS_EXP + BLOCK_SIZE_EXP)) - OFFSET_EXP - 1:0] tag; //Modified
     reg [CACHE_SIZE_EXP - (WAYS_EXP + BLOCK_SIZE_EXP)-1:0] index; //Modified
-    //integer index;
+    
     reg [OFFSET_EXP-1:0] offset;
     reg read_hit; //miss= 0
     reg [(32'd1<<WAYS_EXP)-1:0] valid_bit; //Modified
     reg [(32'd1<<WAYS_EXP)-1:0] valid_bit_out; //Modified
     integer j, i, k, a, b, c;
-    // input hit; //Modified
-    // reg [3:0] valid_bit;
+   
 
     reg[31:0] replace;
     integer hit_counter = 32'd0;
@@ -46,8 +39,7 @@ module memory();
     reg[31:0] set0[(32'd1<<WAYS_EXP)-1:0]; //Modified
     // reg[31:0] i = 32'd0;
     reg[31:0] random_ind = 32'd0;
-    // reg[31:0] cache_data_offset_start = 32'd0; //Modified
-    // reg[31:0] cache_data_offset_end = 32'd0; /Modified
+    
 
     initial
     begin
@@ -60,15 +52,13 @@ module memory();
       for (a = 0; a<(32'd1<<WAYS_EXP); a = a + 1) //Modified
       begin
           for (b = 0; b<INDEX_SIZE; b = b+1)
-          begin
-              // $display("b = %b", b);'
-              cache[b][a][CACHE_DATA_SIZE-2] = 0;
-              // $display("cache[b][a][CACHE_DATA_SIZE-2] = %b", cache[b][a][CACHE_DATA_SIZE-2]);
+          begin              
+              cache[b][a][CACHE_DATA_SIZE-2] = 0;             
           end
       end
 
       $readmemh("instruction.txt", instruction_set);
-      // $display(instruction_set[0]);
+    
       for (i=0; i<NO_OF_INST; i=i+1)
         begin
           instn = instruction_set[i];
@@ -79,11 +69,7 @@ module memory();
           index = address[CACHE_SIZE_EXP - BLOCK_SIZE_EXP :OFFSET_EXP];
           offset = address[OFFSET_EXP-1:0];
           read_hit = 0;
-          // valid_bit = 4'b0000;
-          // valid_bit = WAYS'd0;
-          // $display("address: %b", address);
-          // $display("tag: %b", tag);
-
+    
           for(j=0 ; j<32'd1<<WAYS_EXP ; j=j+1)
           begin
             valid_bit[j] = 0;
@@ -95,13 +81,10 @@ module memory();
           for ( j=0; j<(32'd1<<WAYS_EXP); j=j+1) //Modified
           begin
             valid_bit[j] = cache[index][j][CACHE_DATA_SIZE-2];
-            // $display("CACHE_VALID_BIT : %b", cache[index][j][CACHE_DATA_SIZE-2]);
-            // $display("VALID_BIT : %b", valid_bit[j]);
+            
             if (valid_bit[j]==1 && cache[index][j][CACHE_DATA_SIZE-3 -: ADDRESS_SIZE - (CACHE_SIZE_EXP - (WAYS_EXP + BLOCK_SIZE_EXP)) - OFFSET_EXP]==tag) //Modified
               begin
                 read_hit=1;
-                // hit_counter = hit_counter + 1;
-                // $display("\n" + memory[tag]);
               end
           end
           for (k=0; k<(32'd1<<WAYS_EXP); k = k+1) //initialise to 0 //Modified
@@ -129,8 +112,7 @@ module memory();
             begin
               if(read_hit==0)
                 begin
-                  // cache[index][replace]= {memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][0], memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][1], memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][2], memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][3]};
-
+ 
                   for(k=0 ; k<(32'd1<<(BLOCK_SIZE_EXP-3)) ; k=k+1)
                   begin
                     cache[index][replace][(BLOCK_SIZE-1-k*8) -: 8] = memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][k];
@@ -145,12 +127,7 @@ module memory();
                   // $display("It was a hit\n");
                   hit_counter = hit_counter + 1;
                 end
-              // $display(cache[index][j]+ "\n\n");
-              // if (cache[index][j][36:32]==tag)
-              //   begin
-              //     read_hit=1;
-              //     // $display("\n" + memory[tag]);
-              //   end
+              
 
             end
           else
@@ -158,10 +135,6 @@ module memory();
               cache[index][replace][CACHE_DATA_SIZE-2] = 1;
               if(read_hit==0 && cache[index][replace][CACHE_DATA_SIZE-1]==1)
               begin
-                // memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][0] = cache[index][replace][BLOCK_SIZE-1 -: DATA_SIZE];
-                // memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][1] = cache[index][replace][BLOCK_SIZE-DATA_SIZE-1 -: DATA_SIZE];
-                // memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][2] = cache[index][replace][BLOCK_SIZE-(2*DATA_SIZE)-1 -: DATA_SIZE];
-                // memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][3] = cache[index][replace][BLOCK_SIZE-1 -(3*DATA_SIZE) -: DATA_SIZE];
                 for(k=0 ; k<32'd1<<WAYS_EXP ; k=k+1)
                 begin
                   memory[address[ADDRESS_SIZE-1 : OFFSET_EXP]][k] = cache[index][replace][BLOCK_SIZE-(k*DATA_SIZE)-1 -: DATA_SIZE];
